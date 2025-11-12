4404dcfd28bfe1a2d58c4364004e312f3f5b7153
Антон Некрасов <nas_box@rambler.ru>
2025-11-12 12:39:07 +0300

    add v-for

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,36 @@
-<script setup></script>
+<script setup>
+import { reactive } from 'vue'
+
+const basket = reactive([
+  {
+    id: 1,
+    name: 'Blue Flower Print Crop Top',
+    color: 'Yellow',
+    size: 'M',
+    price: '$29.00',
+    quantity: 1,
+    imageUrl: '../public/crop-top.png',
+  },
+  {
+    id: 2,
+    name: 'Levender Hoodie',
+    color: 'Levender',
+    size: 'XXL',
+    price: '$119.00',
+    quantity: 1,
+    imageUrl: '../public/hoodie.png',
+  },
+  {
+    id: 3,
+    name: 'Black Sweatshirt',
+    color: 'Black',
+    size: 'XXL',
+    price: '$123.00',
+    quantity: 1,
+    imageUrl: '../public/sweatshirt.png',
+  },
+])
+</script>
 
 <template>
   <div class="container basket">
@@ -13,21 +45,21 @@
         </tr>
       </thead>
       <tbody class="basket-table__body">
-        <tr>
+        <tr v-for="product in basket" :key="product.id">
           <td>
             <div class="basket-item">
               <div class="basket-item__image">
-                <img src="../public/crop-top.png" alt="" />
+                <img :src="`${product.imageUrl}`" alt="" />
               </div>
               <div class="basket-item__info">
-                <h2 class="basket-item__info-h2">Blue Flower Print Crop Top</h2>
-                <p class="basket-item__info-p">Color: Yellow</p>
-                <p class="basket-item__info-p">Size: M</p>
+                <h2 class="basket-item__info-h2">{{ product.name }}</h2>
+                <p class="basket-item__info-p">{{ product.color }}</p>
+                <p class="basket-item__info-p">{{ product.size }}</p>
               </div>
             </div>
           </td>
           <td>
-            <p class="basket-item__price">$29.00</p>
+            <p class="basket-item__price">{{ product.price }}</p>
           </td>
           <td>
             <div class="basket-item__quantity">
@@ -61,105 +93,6 @@
             </button>
           </td>
         </tr>
-
-        <tr>
-          <td>
-            <div class="basket-item">
-              <div class="basket-item__image">
-                <img src="../public/hoodie.png" alt="" />
-              </div>
-              <div class="basket-item__info">
-                <h2 class="basket-item__info-h2">Levender Hoodie</h2>
-                <p class="basket-item__info-p">Color: Levender</p>
-                <p class="basket-item__info-p">Size: XXL</p>
-              </div>
-            </div>
-          </td>
-          <td>
-            <p class="basket-item__price">$119.00</p>
-          </td>
-          <td>
-            <div class="basket-item__quantity">
-              <button class="quantity-button">–</button>
-              <input type="number" value="1" min="1" inputmode="numeric" />
-              <button class="quantity-button">+</button>
-            </div>
-          </td>
-          <td>
-            <p class="basket-item__price">$119.00</p>
-          </td>
-          <td>
-            <button class="btn btn-delete" aria-label="Удалить">
-              <svg
-                class="w-6 h-6 text-gray-800 dark:text-white"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
-                />
-              </svg>
-            </button>
-          </td>
-        </tr>
-
-        <tr>
-          <td>
-            <div class="basket-item">
-              <div class="basket-item__image">
-                <img src="../public/sweatshirt.png" alt="" />
-              </div>
-              <div class="basket-item__info">
-                <h2 class="basket-item__info-h2">Black Sweatshirt</h2>
-                <p class="basket-item__info-p">Color: Black</p>
-                <p class="basket-item__info-p">Size: XXL</p>
-              </div>
-            </div>
-          </td>
-          <td>
-            <p class="basket-item__price">$123.00</p>
-          </td>
-          <td>
-            <div class="basket-item__quantity">
-              <button class="quantity-button">–</button>
-              <input type="number" value="1" min="1" inputmode="numeric" />
-              <button class="quantity-button">+</button>
-            </div>
-          </td>
-          <td>
-            <p class="basket-item__price">$123.00</p>
-          </td>
-          <td>
-            <button class="btn btn-delete" aria-label="Удалить">
-              <svg
-                class="w-6 h-6 text-gray-800 dark:text-white"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
-                />
-              </svg>
-            </button>
-          </td>
-        </tr>
-
         <tr>
           <td colspan="5">
             <p class="basket-table__empty">No items</p>
