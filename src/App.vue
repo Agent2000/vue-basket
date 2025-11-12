125161d9788b37fc1116691ac491f44332451d6d
Антон Некрасов <nas_box@rambler.ru>
2025-11-12 12:08:49 +0300

    add template from arch files

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,182 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container basket">
+    <table class="basket-table">
+      <thead class="basket-table__header">
+        <tr>
+          <th>Product Details</th>
+          <th>Price</th>
+          <th>Quantity</th>
+          <th>Subtotal</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody class="basket-table__body">
+        <tr>
+          <td>
+            <div class="basket-item">
+              <div class="basket-item__image">
+                <img src="../public/crop-top.png" alt="" />
+              </div>
+              <div class="basket-item__info">
+                <h2 class="basket-item__info-h2">Blue Flower Print Crop Top</h2>
+                <p class="basket-item__info-p">Color: Yellow</p>
+                <p class="basket-item__info-p">Size: M</p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <p class="basket-item__price">$29.00</p>
+          </td>
+          <td>
+            <div class="basket-item__quantity">
+              <button class="quantity-button">–</button>
+              <input type="number" value="1" min="1" />
+              <button class="quantity-button">+</button>
+            </div>
+          </td>
+          <td>
+            <p class="basket-item__price">$29.00</p>
+          </td>
+          <td>
+            <button class="btn btn-delete" aria-label="Удалить">
+              <svg
+                class="w-6 h-6 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
+                />
+              </svg>
+            </button>
+          </td>
+        </tr>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+        <tr>
+          <td>
+            <div class="basket-item">
+              <div class="basket-item__image">
+                <img src="../public/hoodie.png" alt="" />
+              </div>
+              <div class="basket-item__info">
+                <h2 class="basket-item__info-h2">Levender Hoodie</h2>
+                <p class="basket-item__info-p">Color: Levender</p>
+                <p class="basket-item__info-p">Size: XXL</p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <p class="basket-item__price">$119.00</p>
+          </td>
+          <td>
+            <div class="basket-item__quantity">
+              <button class="quantity-button">–</button>
+              <input type="number" value="1" min="1" inputmode="numeric" />
+              <button class="quantity-button">+</button>
+            </div>
+          </td>
+          <td>
+            <p class="basket-item__price">$119.00</p>
+          </td>
+          <td>
+            <button class="btn btn-delete" aria-label="Удалить">
+              <svg
+                class="w-6 h-6 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
+                />
+              </svg>
+            </button>
+          </td>
+        </tr>
 
-  <main>
-    <TheWelcome />
-  </main>
+        <tr>
+          <td>
+            <div class="basket-item">
+              <div class="basket-item__image">
+                <img src="../public/sweatshirt.png" alt="" />
+              </div>
+              <div class="basket-item__info">
+                <h2 class="basket-item__info-h2">Black Sweatshirt</h2>
+                <p class="basket-item__info-p">Color: Black</p>
+                <p class="basket-item__info-p">Size: XXL</p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <p class="basket-item__price">$123.00</p>
+          </td>
+          <td>
+            <div class="basket-item__quantity">
+              <button class="quantity-button">–</button>
+              <input type="number" value="1" min="1" inputmode="numeric" />
+              <button class="quantity-button">+</button>
+            </div>
+          </td>
+          <td>
+            <p class="basket-item__price">$123.00</p>
+          </td>
+          <td>
+            <button class="btn btn-delete" aria-label="Удалить">
+              <svg
+                class="w-6 h-6 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
+                />
+              </svg>
+            </button>
+          </td>
+        </tr>
+
+        <tr>
+          <td colspan="5">
+            <p class="basket-table__empty">No items</p>
+          </td>
+        </tr>
+
+        <tr>
+          <td colspan="5">
+            <div class="basket-table__summary">
+              <p class="basket-table__total">Total <b>$271.00</b></p>
+              <p>Tax $27.10</p>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style src="./App.css"></style>
